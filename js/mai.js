9c39a12f6ba95c9395aa361673f14e783f602b90
Anacleto Francisco <107069897+Strongadas@users.noreply.github.com>
2022-12-09 16:14:42 +0200

    ok

---
+++ js/mai.js
@@ -0,0 +1,10 @@
+const ccv = document.querySelector(".btn-abrir-formulario")
+const pic = document.querySelector("trasera")
+
+btn.addEventListener("click",change)
+
+function change (){
+    pic.src = "../img/bg-tarjeta/bg-tarjeta-03.jpg"
+}
+
+
